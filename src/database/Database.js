ebc42be13db5cd42a35b1c2c710aeeb197940ca8
Raphael2908 <81660932+Raphael2908@users.noreply.github.com>
2021-10-12 10:23:08 +0800

    feat: Updated Db

--- src/database/Database.js
+++ src/database/Database.js
@@ -2,14 +2,14 @@ const Database = {
 	// Halo 
 	ZetaHalo: {
     name: "Zeta Halo",
-    temperature: "19",
+    temperature: 19,
     season: "Auttum",
     img: "https://cdn.mos.cms.futurecdn.net/pXxzs8aTb7dXSyNtUaRcQf.jpg",
     origin: "Halo"
   },
   Reach: {
     name: "Reach",
-    temperature: "25",
+    temperature: 25,
     season: "Spring",
     img: "https://i2.wp.com/haruspis.blog/wp-content/uploads/2016/11/re5.png?fit=1920%2C1080&ssl=1",
     origin: "Halo"
@@ -17,11 +17,40 @@ const Database = {
 	// Star Wars
 	Tattooine: {
     name: "Tattoine",
-    temperature: "34",
+    temperature: 34,
     season: "Summer",
     img: "https://img.wallpapersafari.com/desktop/1920/1080/32/51/bvneSQ.jpg",
     origin: "Star Wars"
-  }
+  },
+  // Lord of the Rings
+  MiddleEarth:{
+    name: "Middle Earth",
+    temperature: 32,
+    season: "Summer",
+    img: "https://img.wallpapersafari.com/desktop/1920/1080/35/21/8rXJYz.png",
+    origin: "Lord of the Rings"
+  },
+  // God Of War
+  Olympus: {
+    name: "Olympus",
+    temperature: 13,
+    season: "Auttum",
+    img: "https://img.wallpapersafari.com/desktop/1920/1080/99/84/2PoGc7.jpg"
+  },
+  // Destiny
+  Europa: {
+    name: "Europa",
+    temperature: -220,
+    season: "Space",
+    img: "https://d1lss44hh2trtw.cloudfront.net/assets/article/2020/11/11/reclaiming-europa-quest-destiny-2_feature.jpg"
+  },
+  // Greek Mythology
+  Asgard: {
+    name: "Asgard",
+    temperature: 7.5,
+    season: "Winter",
+    img: "https://d1lss44hh2trtw.cloudfront.net/assets/article/2020/11/11/reclaiming-europa-quest-destiny-2_feature.jpg"
+  } 
 }
 
 export { Database }
