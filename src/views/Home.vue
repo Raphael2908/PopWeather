524093320833d5107b2ff99435e41ff97d4efc9b
Raphael2908 <81660932+Raphael2908@users.noreply.github.com>
2021-10-13 10:00:04 +0800

    feat: ui improvements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
 <main class="h-full w-full bg-no-repeat bg-cover " :style="{ backgroundImage: 'url(' + img + ')' }">
    
-  <section class="flex flex-row justify-center">
+  <section class="flex flex-row justify-center content-center	">
     <div class="flex flex-col">
       <search @search="doSearch"></search>
       <Card> 
-        <div class="flex flex-row justify-around"> 
-          <h1 class="font-extrabold text-9xl font-mono ">{{ actualWeather.temp_c }}°C </h1>
-          <div class="pl-5 flex flex-col justify-center">
-            <h3 class=""> {{ place.name }} </h3>
-            <h3> {{ place.season }} </h3>
-            <h3>{{ Searchedlocation }}</h3>
+        <div class="flex flex-row justify-center flex-wrap "> 
+            <h1 class="font-extrabold text-8xl sm:text-9xl font-mono ">{{ actualWeather.temp_c }}°C </h1>
+          <div class="pl-5 flex flex-col justify-center text-left">
+            <h3 class="font-semibold"> {{ Searchedlocation }} </h3>
+            <p>feels like {{ place.name }},</p>
+            <h3> {{ actualWeatherCon.text }}? </h3>
           </div>
         </div>
       </Card>
@@ -37,8 +37,8 @@ export default {
       places: Database,
       img: "",
       place: Object,
-      actualLocation: Object,
-      actualWeather: Object
+      actualWeather: Object,
+      actualWeatherCon:Object,
     }
   },
   mounted(){
@@ -70,15 +70,14 @@ export default {
             // Return Importan Variables
             this.place = popLocation
             this.img = popLocation.img
-            this.actualLocation = weatherData.location
             this.actualWeather = weatherData.current
+            this.actualWeatherCon = weatherData.current.condition
             return
           }
           else {
             continue
           }
         }
-
       });
     }, 
     doSearch(event){
